a8ac828627299bfe301439c48c8cc54290903f3a
ChrisEzikeJL <chris@joint.cool>
2023-03-24 00:32:31 +0100

    feat: Complete Task App

--- src/index.ts
+++ src/index.ts
@@ -16,11 +16,32 @@ interface Task {
 /// Capture Task Data ///
 taskForm?.addEventListener('submit', function (e) {
   e.preventDefault();
-  if (taskTitle?.value == String || taskTitle?.value == null) return;
-  const newTask: Task = {
-    id: uuidV4(),
-    title: taskTitle.value,
-    completed: false,
-    createdAt: new Date(),
-  };
+  if (!taskTitle?.value) {
+    alert('Please Add Task Title');
+  } else {
+    const newTask: Task = {
+      id: uuidV4(),
+      title: taskTitle.value,
+      completed: false,
+      createdAt: new Date(),
+    };
+    addItemToTask(newTask);
+  }
+  return;
 });
+
+// Add Task
+const addItemToTask = function (task: Task): void {
+  const item = document.createElement('li');
+  const label = document.createElement('label');
+  const checkbox = document.createElement('input');
+  checkbox.addEventListener('change', function () {
+    task.completed = checkbox.checked;
+    if (task.completed) {
+      label.style.textDecoration = 'line-through';
+    }
+  });
+  label.append(checkbox, task.title);
+  item.append(label);
+  list?.append(item);
+};
